60c4812162978990e8af51a8bc5c0b23b5939ef2
shsdev <sschlarb@gmail.com>
2016-09-16 16:05:54 +0200

    #16 added parsing and serialization of isPartOf
    Change-Id: I6f71224a4db56b3424e37dc9f8d935225430d79a

--- entity-definitions/src/main/java/eu/europeana/entity/definitions/model/ResourcePreview.java
+++ entity-definitions/src/main/java/eu/europeana/entity/definitions/model/ResourcePreview.java
@@ -1,5 +1,13 @@
 package eu.europeana.entity.definitions.model;
 
 public interface ResourcePreview {
+	
+	public String getHttpUri();
+	
+	public void setHttpUri(String httpUri);
+	
+	public String getPrefLabel();
+	
+	public void setPrefLabel(String prefLabel);
 
 }

c22abf5e5f6e60039b779f5134eb80d611c5d154
shsdev <sschlarb@gmail.com>
2016-11-25 17:18:09 +0100

    #25 updated fields names
    Change-Id: I885a1228511f60ffbfaa37237eed79770e617350

--- entity-definitions/src/main/java/eu/europeana/entity/definitions/model/vocabulary/AgentSolrFields.java
+++ entity-definitions/src/main/java/eu/europeana/entity/definitions/model/vocabulary/AgentSolrFields.java
@@ -2,16 +2,16 @@ package eu.europeana.entity.definitions.model.vocabulary;
 
 public interface AgentSolrFields extends ConceptSolrFields{
 		
-	public static final String DATE                     = "skos_date";
+	public static final String DATE                     = "dc_date";
 	public static final String BEGIN                    = "edm_begin";
 	public static final String END                      = "edm_end";
-	public static final String HAS_MET                  = "skos_hasMet";
-	public static final String IS_RELATED_TO            = "skos_isRelatedTo";
-	public static final String NAME                     = "skos_name";
+	public static final String HAS_MET                  = "edm_hasMet";
+	public static final String IS_RELATED_TO            = "edm_isRelatedTo";
+	public static final String NAME                     = "foaf_name";
 	public static final String BIOGRAPHICAL_INFORMATION = "rdagr2_biographicalInformation"; 
 	public static final String BIOGRAPHICAL_INFORMATION_ALL = "rdagr2_biographicalInformation"+".*"; 
-	public static final String DATE_OF_BIRTH            = "rdagr2_dateOfBirth";
-	public static final String DATE_OF_DEATH            = "rdagr2_dateOfDeath";
+	public static final String DATE_OF_BIRTH_ALL            = "rdagr2_dateOfBirth"+".*";
+	public static final String DATE_OF_DEATH_ALL            = "rdagr2_dateOfDeath"+".*";
 	public static final String PLACE_OF_BIRTH           = "rdagr2_placeOfBirth";
 	public static final String PLACE_OF_DEATH           = "rdagr2_placeOfDeath";
 	public static final String PLACE_OF_BIRTH_ALL       = "rdagr2_placeOfBirth"+ ".*";

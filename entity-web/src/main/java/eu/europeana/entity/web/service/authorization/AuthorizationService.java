dd93f2ecea576aa8620d9faa938972602ee65580
gsergiu <sergiu.gordea@ait.ac.at>
2019-11-25 13:19:20 +0100

    #142 removed redundant code

--- entity-web/src/main/java/eu/europeana/entity/web/service/authorization/AuthorizationService.java
+++ entity-web/src/main/java/eu/europeana/entity/web/service/authorization/AuthorizationService.java
@@ -1,24 +1,8 @@
 package eu.europeana.entity.web.service.authorization;
 
-import eu.europeana.api.commons.web.exception.ApplicationAuthenticationException;
 import eu.europeana.entity.config.EntityConfiguration;
-import eu.europeana.entity.definitions.model.Agent;
-import eu.europeana.entity.web.exception.authorization.OperationAuthorizationException;
-import eu.europeana.entity.web.exception.authorization.UserAuthorizationException;
 
 public interface AuthorizationService extends eu.europeana.api.commons.service.authorization.AuthorizationService{
-
-	/**
-	 * use authorizeUser(String userToken, String apiKey, String operationName) 
-	 * @param userToken
-	 * @param apiKey
-	 * @param entityId The entity id string
-	 * @param operationName
-	 * @return
-	 * @throws UserAuthorizationException
-	 */
-	Agent authorizeUser(String userToken, String apiKey, String entityId, String operationName)
-			throws UserAuthorizationException, ApplicationAuthenticationException, OperationAuthorizationException;
 
 	EntityConfiguration getConfiguration();
 	
--- entity-web/src/main/java/eu/europeana/entity/web/service/authorization/AuthorizationServiceImpl.java
+++ entity-web/src/main/java/eu/europeana/entity/web/service/authorization/AuthorizationServiceImpl.java
@@ -4,19 +4,11 @@ import javax.annotation.Resource;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-import org.springframework.http.HttpStatus;
 import org.springframework.security.oauth2.provider.ClientDetailsService;
 
-import eu.europeana.api.common.config.I18nConstants;
 import eu.europeana.api.commons.definitions.vocabulary.Role;
 import eu.europeana.api.commons.service.authorization.BaseAuthorizationService;
-import eu.europeana.api.commons.web.exception.ApplicationAuthenticationException;
 import eu.europeana.entity.config.EntityConfiguration;
-import eu.europeana.entity.definitions.model.Agent;
-import eu.europeana.entity.definitions.model.authentication.Application;
-import eu.europeana.entity.web.exception.authorization.OperationAuthorizationException;
-import eu.europeana.entity.web.exception.authorization.UserAuthorizationException;
-import eu.europeana.entity.web.model.vocabulary.Operations;
 import eu.europeana.entity.web.model.vocabulary.UserRoles;
 import eu.europeana.entity.web.service.authentication.AuthenticationService;
 
@@ -47,78 +39,6 @@ public class AuthorizationServiceImpl extends BaseAuthorizationService implement
 
     public void setAuthenticationService(AuthenticationService authenticationService) {
 	this.authenticationService = authenticationService;
-    }
-
-    /*
-     * (non-Javadoc)
-     * 
-     * @see
-     * eu.europeana.set.web.service.authorization.AuthorizationService#authorizeUser
-     * (java.lang.String, java.lang.String,
-     * eu.europeana.set.definitions.model.UserSetId, java.lang.String)
-     */
-    @Override
-    public Agent authorizeUser(String userToken, String apiKey, String setId, String operationName)
-	    throws UserAuthorizationException, ApplicationAuthenticationException, OperationAuthorizationException {
-
-	Application app = getAuthenticationService().getByApiKey(apiKey);
-	Agent user = getAuthenticationService().getUserByToken(apiKey, userToken);
-
-	// NOTE: actually getUserByToken throws exception when user is not found
-//		if (user== null || user.getName() == null || user.getUserGroup() == null)
-//			throw new UserAuthorizationException(I18nConstants.INVALID_TOKEN, 
-//					I18nConstants.INVALID_TOKEN, new String[]{userToken}, HttpStatus.UNAUTHORIZED);
-
-	if (!isAdmin(user) && !hasPermission(app, setId, operationName))
-	    throw new OperationAuthorizationException(I18nConstants.USER_NOT_AUTHORIZED,
-		    I18nConstants.USER_NOT_AUTHORIZED, new String[] { "Entity id: " + userToken },
-		    HttpStatus.UNAUTHORIZED);
-
-	// check permissions
-	if (isAdmin(user) && hasPermission(user, operationName))// allow all
-	    return user;
-//		else if(isTester(user) && configuration.isProductionEnvironment()){
-//			// testers not allowed in production environment
-//			throw new UserAuthorizationException(I18nConstants.TEST_USER_FORBIDDEN, 
-//					I18nConstants.TEST_USER_FORBIDDEN, new String[]{user.getName()}, HttpStatus.FORBIDDEN);
-//		} else	if(hasPermission(user, operationName)){
-//			//user is authorized
-//			return user;
-//		}
-
-	// user is not authorized to perform operation
-	throw new UserAuthorizationException(I18nConstants.USER_NOT_AUTHORIZED, I18nConstants.USER_NOT_AUTHORIZED,
-		new String[] {}, HttpStatus.UNAUTHORIZED);
-//				I18nConstants.USER_NOT_AUTHORIZED, new String[]{user.getName()}, HttpStatus.UNAUTHORIZED);	
-    }
-
-    // verify client app privileges
-    protected boolean hasPermission(Application app, String setId, String operationName) {
-	if (Operations.MODERATION_ALL.equals(operationName) || Operations.RETRIEVE.equals(operationName)
-		|| Operations.UPDATE.equals(operationName) || Operations.CREATE.equals(operationName))
-	    return true;
-
-	return setId != null;
-    }
-
-    // verify user privileges
-    protected boolean hasPermission(Agent user, String operationName) {
-//		Roles role = Roles.valueOf(user.getUserGroup());
-//		
-//		for (String operation : role.getOperations()) {
-//			if(operation.equalsIgnoreCase(operationName))
-	return true;// users is authorized, everything ok
-//		}
-
-//		return false;
-    }
-
-    protected boolean isAdmin(Agent user) {
-	return true; // Roles.ADMIN.name().equals(user.getUserGroup());
-    }
-
-    protected boolean isTester(Agent user) {
-	return false; // Roles.TESTER.name().equals(user.getUserGroup());
     }
 
     public EntityConfiguration getConfiguration() {
